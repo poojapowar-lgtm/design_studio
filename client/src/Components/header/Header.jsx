0db102ae19e92b8fa865e57490cdf9d24fdf2e02
poojapowar123 <poojapowar57@gmail.com>
2026-08-12 10:48:41 +0530

    added photos to innerpages

--- client/src/Components/header/Header.jsx
+++ client/src/Components/header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
               <Link to={item.to}>
                 {item.key === "home" ? (
                   <img
-                    src="/assets/images/icons/logo.PNG"
+                    src="/assets/images/icons/logo.jpeg"
                     alt="Logo1"
                     className="logo1"
                   />
