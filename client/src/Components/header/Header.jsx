9648e3f6137ed05acc9bf2ab1d7b70d979da693d
poojapowar123 <poojapowar57@gmail.com>
2026-08-13 16:30:49 +0530

    added google map

--- client/src/Components/header/Header.jsx
+++ client/src/Components/header/Header.jsx
@@ -50,18 +50,30 @@ const Header = () => {
           {navMenus.map((item) => (
             <li key={item.key}>
               <Link to={item.to} onClick={closeMenu}>
-
-                {/* Home Icon only on mobile */}
                 {item.key === "home" ? (
                   <IoMdHome className="mobile-home-icon" />
                 ) : (
                   item.title
                 )}
-
               </Link>
             </li>
           ))}
         </ul>
+
+        {/* Mobile Map */}
+        <div className="mobile-map">
+          <p> MIT-ADT Campus, Rajbaugh, Solapur – Pune Highway Near Bharat
+            Petrol Pump, Loni-Kalbhor, Maharashtra – 412201</p>
+          <iframe
+            className="map"
+            title="Map location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.8228655029525!2d74.02029187595625!3d18.49168107005449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2e9784f39a613%3A0x189ffd59802ec685!2sMIT%20School%20of%20Food%20Technology%2C%20MIT%20ADT%20University%2C%20Pune!5e0!3m2!1sen!2sin!4v1773135364412!5m2!1sen!2sin"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </div>
+
       </nav>
 
       {/* Right Logo */}
